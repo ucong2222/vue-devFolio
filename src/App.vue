4017bbc115b0cc454d4344bd26966604cdcb84ea
ucong2222 <dbrudrjf21@gmail.com>
2021-03-13 17:33:07 +0900

    프로젝트 생성 및 안쓰는 소스코드 제거

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
